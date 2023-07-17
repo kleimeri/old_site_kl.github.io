aa9c936f2db26e9d7f98849533655f9510e31277
KLeimer <102779194+kleimeri@users.noreply.github.com>
2023-07-17 16:08:18 +0300

    Add files via upload

---
+++ slider_1/assets/js/slider.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  centeredSlides: true,
+  loop:true,
+  grabCursor: true,
+  initialSlide:1,
+  speed:800,
+  
+  pagination: {
+    el: ".pagination",
+    clickable: true,
+  },
+  navigation: {
+     nextEl: '.arrow__right',
+     prevEl: '.arrow__left',
+   },
+ });
+
+
+ 
